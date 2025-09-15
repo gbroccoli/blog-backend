7d820792d5623b53d8103e03b7edebac4a8edc0a
gbroccoli <vovo.r@ya.ru>
2025-09-15 21:48:02 +1000

    edit

--- main.go
+++ main.go
@@ -17,6 +17,16 @@ func CrossOriginMiddleware(prot *http.CrossOriginProtection) gin.HandlerFunc {
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+		c.Next()
+	}
+}
+
 func main() {
 
 	r := gin.Default()
@@ -26,6 +36,7 @@ func main() {
 	_ = prot.AddTrustedOrigin("http://localhost:5173/")
 
 	r.Use(CrossOriginMiddleware(prot))
+	r.Use(CORSMiddleware())
 
 	api := r.Group("/")
 	{
@@ -37,7 +48,12 @@ func main() {
 
 		posts := api.Group("/posts")
 		{
-			posts.GET("/", func(c *gin.Context) {})
+			posts.GET("", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"message": "Hello World",
+				})
+			})
+			posts.GET("/:id", func(c *gin.Context) {})
 			posts.POST("/create", func(c *gin.Context) {})
 			posts.PUT("/:id/edit", func(c *gin.Context) {})
 			posts.PATCH("/:id/edit", func(c *gin.Context) {})
